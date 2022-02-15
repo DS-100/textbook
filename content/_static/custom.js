aaefb8181fc37ac9f2d93b80fbc9371ddeb908a1
Sam Lau <samlau95@gmail.com>
2022-02-15 15:14:05 -0800

    10.2

--- content/_static/custom.js
+++ content/_static/custom.js
@@ -1,6 +1,9 @@
-// configure require.js to load plotly from cdn
-var require = {
-  paths: {
-    plotly: 'https://cdn.plot.ly/plotly-2.9.0.min',
-  },
-}
+// configure require.js to load plotly from cdn. this is commented out since we
+// aren't supporting interactive plotly plots right now, but if we want to
+// support this in the future be sure to uncomment out the code below.
+
+// var require = {
+//   paths: {
+//     plotly: 'https://cdn.plot.ly/plotly-2.9.0.min',
+//   },
+// }
