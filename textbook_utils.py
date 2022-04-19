47699badf7ae24738fa3242badf0d4da7270df64
Sam Lau <samlau95@gmail.com>
2022-04-19 15:44:09 -0700

    Update ch6 and ch7

---
+++ textbook_utils.py
@@ -0,0 +1,91 @@
+"""
+Imports and functions that all textbook pages load by default.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, fixed, interact_manual
+from IPython import get_ipython
+from IPython.display import display, set_matplotlib_formats
+import myst_nb
+
+import plotly
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.io as pio
+
+# set up plotly defaults
+pio.renderers.default = "plotly_mimetype+svg"
+pio.templates["book"] = go.layout.Template(
+    layout=dict(
+        margin=dict(l=10, r=10, t=10, b=10),
+        autosize=True,
+        width=350,
+        height=250,
+        xaxis=dict(showgrid=True),
+        yaxis=dict(showgrid=True),
+        title=dict(x=0.5, xanchor="center"),
+    )
+)
+pio.templates.default = "simple_white+book"
+
+# set up matplotlib defaults
+get_ipython().run_line_magic("matplotlib", "inline")
+set_matplotlib_formats("svg")
+sns.set_style("whitegrid")
+plt.rcParams["figure.figsize"] = (4, 3)
+
+# display options for numpy and pandas
+np.set_printoptions(threshold=20, precision=2, suppress=True)
+pd.set_option("display.max_rows", 7)
+pd.set_option("display.max_columns", 8)
+pd.set_option("precision", 2)
+# stops scientific notation for pandas
+# pd.set_option('display.float_format', '{:.2f}'.format)
+
+
+def display_df(
+    df, rows=pd.options.display.max_rows, cols=pd.options.display.max_columns
+):
+    """Displays n rows and cols from df"""
+    with pd.option_context("display.max_rows", rows, "display.max_columns", cols):
+        display(df)
+
+
+##############################################################################
+# Plotly
+##############################################################################
+
+_clear = ["bingroup"]
+
+
+def _clear_prop(trace, prop):
+    if hasattr(trace, prop):
+        trace.update({prop: None})
+
+
+def _clear_props(traces):
+    for trace in traces:
+        for prop in _clear:
+            _clear_prop(trace, prop)
+
+
+def left_right(left, right, width=700, height=250, **kwargs):
+    """Combine two plotly figures side by side"""
+    fig = make_subplots(cols=2, **kwargs)
+    fig.update_layout(width=width, height=height)
+
+    t1 = next(left.select_traces())
+    t2 = next(right.select_traces())
+    _clear_props([t1, t2])
+    fig.add_trace(t1, row=1, col=1)
+    fig.add_trace(t2, row=1, col=2)
+    return fig
+
+
+def margin(fig, **kwargs):
+    """Set margins for a plotly figure"""
+    return fig.update_layout(margin=kwargs)
