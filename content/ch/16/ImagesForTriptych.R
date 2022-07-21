ba3d9f4d1632710d8265bffaf41f3580c7774c23
Deborah Nolan <nolan@stat.berkeley.edu>
2022-07-21 09:19:26 -0700

    additional sections for inf and pred chapter

--- content/ch/16/ImagesForTriptych.R
+++ content/ch/16/ImagesForTriptych.R
@@ -41,3 +41,40 @@ line = par(lwd=2, mar=c(0.2,0.2,0.2,0.2))
 hist(zB,
      axes = FALSE, xlab="", ylab="", main="")
 dev.off()
+
+
+x_rank = 1:200
+
+png(file="popHistRank.png", width=600, height=350)
+line = par(lwd=2, mar=c(2,0.2,0.2,0.2))
+hist(x_rank, breaks = seq(0.5,200.5,5),
+     axes = FALSE, xlab="", ylab="", main="", xlim = c(0,200))
+axis(1)
+#abline(a=0, b=0, col="black", lwd=2)
+dev.off()
+
+wiki = read.csv("~/textbook/content/datasets/Wikipedia.csv")
+postprod = wiki$postproductivity
+rankX = rank(postprod, ties.method = "average")
+rankTreat = rankX[wiki$experiment==1]
+
+png(file="sampHistRank.png", width=600, height=350)
+line = par(lwd=2, mar=c(2,0.2,0.2,0.2))
+hist(rankTreat, breaks = seq(0.5,200.5,5),
+     axes = FALSE, xlab="", ylab="", main="", xlim = c(0,200))
+axis(1)
+#abline(a=0, b=0, col="black", lwd=2)
+dev.off()
+
+mean(rankTreat)
+sd(rankTreat)
+
+set.seed(42)
+zB = replicate(100000, mean(sample(x_rank, 100)))
+png(file="samplingDistRank.png", width=600, height=350)
+line = par(lwd=2, mar=c(2,0.2,0.2,0.2))
+hist(zB, breaks = seq(83.5,117.5,1),
+     axes = FALSE, xlab="", ylab="", main="")
+axis(1)
+dev.off()
+
